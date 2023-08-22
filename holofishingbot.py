200cac85806f3f15f65570a4f305aa58431a9060
ShizeMation <1732145@gmail.com>
2023-08-22 18:16:35 -0400

    constants

--- holofishingbot.py
+++ holofishingbot.py
@@ -3,12 +3,20 @@ import cv2 as cv
 import numpy as np
 import pydirectinput as di
 
+UP_KEY = 'w'
+DOWN_KEY = 's'
+LEFT_KEY = 'a'
+RIGHT_KEY = 'd'
+CONFIRM_KEY = 'e'
+
 INDICATOR = cv.imread('assets/indicator.png', cv.IMREAD_GRAYSCALE)
 UP = cv.imread('assets/up.png', cv.IMREAD_GRAYSCALE)
 DOWN = cv.imread('assets/down.png', cv.IMREAD_GRAYSCALE)
 LEFT = cv.imread('assets/left.png', cv.IMREAD_GRAYSCALE)
 RIGHT = cv.imread('assets/right.png', cv.IMREAD_GRAYSCALE)
 CIRCLE = cv.imread('assets/circle.png', cv.IMREAD_GRAYSCALE)
+
+MATCH_METHOD = cv.TM_CCOEFF_NORMED
 
 isFishing = False
 
@@ -20,30 +28,30 @@ while (True):
         target = np.array(sct.grab(targetDim))
 
     indicator = cv.cvtColor(indicator, cv.COLOR_BGRA2GRAY)
-    resIndicator = cv.matchTemplate(indicator, INDICATOR, cv.TM_CCOEFF_NORMED)
+    resIndicator = cv.matchTemplate(indicator, INDICATOR, MATCH_METHOD)
 
     if (cv.minMaxLoc(resIndicator)[1] > 0.9):
         isFishing = True
     else:
         isFishing = False
-        di.press("e")
+        di.press(CONFIRM_KEY)
 
     if isFishing:
         target = cv.cvtColor(target, cv.COLOR_BGRA2GRAY)
 
-        resUp = cv.matchTemplate(target, UP, cv.TM_CCOEFF_NORMED)
-        resDown = cv.matchTemplate(target, DOWN, cv.TM_CCOEFF_NORMED)
-        resLeft = cv.matchTemplate(target, LEFT, cv.TM_CCOEFF_NORMED)
-        resRight = cv.matchTemplate(target, RIGHT, cv.TM_CCOEFF_NORMED)
-        resCircle = cv.matchTemplate(target, CIRCLE, cv.TM_CCOEFF_NORMED)
+        resUp = cv.matchTemplate(target, UP, MATCH_METHOD)
+        resDown = cv.matchTemplate(target, DOWN, MATCH_METHOD)
+        resLeft = cv.matchTemplate(target, LEFT, MATCH_METHOD)
+        resRight = cv.matchTemplate(target, RIGHT, MATCH_METHOD)
+        resCircle = cv.matchTemplate(target, CIRCLE, MATCH_METHOD)
 
         if (cv.minMaxLoc(resUp)[1] > 0.9):
-            di.press("w")
+            di.press(UP_KEY)
         elif (cv.minMaxLoc(resDown)[1] > 0.9):
-            di.press("s")
+            di.press(DOWN_KEY)
         elif (cv.minMaxLoc(resLeft)[1] > 0.9):
-            di.press("a")
+            di.press(LEFT_KEY)
         elif (cv.minMaxLoc(resRight)[1] > 0.9):
-            di.press("d")
+            di.press(RIGHT_KEY)
         elif (cv.minMaxLoc(resCircle)[1] > 0.9):
-            di.press("e")
+            di.press(CONFIRM_KEY)
